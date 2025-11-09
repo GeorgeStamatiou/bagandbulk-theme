3ce1a5a9bfca9dc9265bd7fd1590480e881416a5
George Stamatiou <gstamatiou7@gmail.com>
2025-11-09 12:48:34 +0200

    Added traditional CSS structure and linked main.css

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 function bagandbulk_enqueue_styles() {
+  // Main theme stylesheet
   wp_enqueue_style('bagandbulk-style', get_stylesheet_uri());
+  // Custom CSS file
+  wp_enqueue_style('bagandbulk-main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'bagandbulk_enqueue_styles');
 
